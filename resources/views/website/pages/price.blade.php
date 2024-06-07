7fb52aead3cb8b277e8bfc4336e614595d20fc01
ahmed ali <44751885+AhmedAliSayed13@users.noreply.github.com>
2024-06-07 19:00:40 +0300

    price page con

--- resources/views/website/pages/price.blade.php
+++ resources/views/website/pages/price.blade.php
@@ -1,7 +1,8 @@
 @extends('website.layouts.master')
-
 @section('style')
-
+<link rel="stylesheet" type="text/css" href="{{asset('users-asset/vendors/css/extensions/nouislider.min.css')}}">
+<link rel="stylesheet" type="text/css"
+    href="{{asset('users-asset/css-rtl/plugins/extensions/ext-component-sliders.css')}}">
 @endsection
 
 
@@ -32,8 +33,11 @@
                     <div class="details-div">
                         <p class="mt-2">حتى </p>
                         <p class="mt-3"><span>100</span> فيديو </p>
-                        <p class="mt-1">$500</p>
-                        <p class="mt-1">" إضف إلى الرصيد: 500 دولار"</p>
+                        <p class="mt-1">$<b class="slider-value">500</b></p>
+                        <p class="mt-1"> إضف إلى الرصيد:
+                            <b class="slider-value">500</b>
+                            دولار
+                        </p>
                         <div class="text-center mt-2">
                             <a class="btn btn-relief-primary mr-3 ml-3" href="#"
                                 role="button">{{__('website.orderNow')}}</a>
@@ -43,8 +47,71 @@
 
                 </div>
                 <div class="col-8 select-div">
-                    11111
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="price-slider">
+                                <header class="price-title">حدد ميزانيتك.</header>
+                                <div class="price-slider-body">
+                                    <div class="range-slider mt-2" id="price-slider"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" col-3">
+                            <div class="price-slider-result bg-div-gray">
 
+                                <header>التوفير النقدي
+
+                                    <i data-feather='info'></i>
+                                </header>
+                                <p class="color-text">$35,972</p>
+                            </div>
+                        </div>
+                        <div class=" col-3">
+                            <div class="price-slider-result bg-div-gray">
+
+                                <header>التوفير النقدي
+                                    <i data-feather='info'></i>
+                                </header>
+                                <p class="color-text">$35,972</p>
+                            </div>
+                        </div>
+                        <div class=" col-3">
+                            <div class="price-slider-result bg-div-gray">
+                                <header>التوفير النقدي
+                                    <i data-feather='info'></i>
+                                </header>
+                                <p class="color-text">$35,972</p>
+                            </div>
+                        </div>
+                        <div class=" col-3">
+                            <div class="price-slider-result bg-div-gray">
+                                <header>التوفير النقدي
+                                    <i data-feather='info'></i>
+                                </header>
+                                <p class="color-text">$35,972</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="row price-div3">
+        <div class="col-8 mt-3 m-auto">
+            <div class="row">
+                <div class="col-12 bg-div-gray">
+                    <header></header>
 
                 </div>
             </div>
@@ -57,5 +124,49 @@
 
 
 @section('script')
+<script src="{{asset('users-asset/vendors/js/extensions/wNumb.min.js')}}"></script>
+<script src="{{asset('users-asset/vendors/js/extensions/nouislider.min.js')}}"></script>
+{{-- <script src="{{asset('users-asset/js/scripts/pages/app-ecommerce.js')}}"></script> --}}
+<script>
+    priceSlider = document.getElementById('price-slider');
 
+                noUiSlider.create(priceSlider, {
+      start: [500], // Initial value
+      range: {
+        'min': [500],
+        '20%': [1000],
+        '40%': [2500],
+        '60%': [5000],
+        '80%': [10000],
+        'max': [15000]
+      },
+      snap: true,
+      format: {
+        to: function (value) {
+          return value.toFixed(0);
+        },
+        from: function (value) {
+          return Number(value);
+        }
+      },
+      pips: {
+        mode: 'values',
+        values: [500, 1000, 2500, 5000, 10000, 15000],
+        density: 20 // Hide density pips
+      }
+    });
+
+    // Display the current value
+
+//    $('.slider-value').noUiSlider.on('update', function (values, handle) {
+//       sliderValue.innerHTML = values[handle];
+//     });
+var sliderValues = document.getElementsByClassName('slider-value');
+    priceSlider.noUiSlider.on('update', function (values, handle) {
+      for (var i = 0; i < sliderValues.length; i++) {
+        sliderValues[i].innerHTML = values[handle];
+      }
+    });
+
+</script>
 @endsection
